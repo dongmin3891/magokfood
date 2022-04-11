d849d311bc072f4b9bf7f42ba3067e0f75c929fe
dongmin <rlaehdals753@naver.com>
2022-04-12 01:25:36 +0900

    feat : home router add

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import {Route, Routes} from 'react-router';
 import GlobalStyles from './GlobalStyles';
-import Header from './components/home/Header';
-import Login from './components/login/Login';
-
+// import Header from './components/home/Header';
+// import Login from './components/login/Login';
+import Navigation from './components/home/Navigation';
 
 
 const Main = () => {
   return (
     <>
-        <GlobalStyles />
-        <Header />
-        <Login />
+        <GlobalStyles /> 
+        {/* <Header />
+        <Login />  */}
+        <Routes>
+            <Route path='/home' element={<Navigation />}/>
+        </Routes>
+
     </>
   );
 }
