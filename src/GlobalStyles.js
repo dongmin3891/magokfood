e3422c1080b43907f53d2c494650ab764c566b41
dongmin <rlaehdals753@naver.com>
2022-04-13 02:03:29 +0900

    style : background-color change

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -13,7 +13,9 @@ const GlobalStyles = createGlobalStyle`
     body {
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 14px;
+        background-color: white;
     }
+    
 `;
 
 export default GlobalStyles;
